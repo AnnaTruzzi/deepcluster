503435c0cc60a934ee71a0253c18b4b151cd7d97
Anna Truzzi <anna.truzzi91@gmail.com>
2019-09-16 16:27:22 +0000

    updates on pretrained model usage

--- DC_pretrained.py
+++ DC_pretrained.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import pickle
 import time
+from collections import OrderedDict
 
 import faiss
 import numpy as np
@@ -20,22 +21,138 @@ import clustering
 import models
 from util import AverageMeter, Logger, UnifLabelSampler
 
-modelpth = '/home/CUSACKLAB/annatruzzi/deepcluster_models/alexnet'
-model = torch.load(modelpth+'model.caffemodel')
+parser = argparse.ArgumentParser(description='PyTorch Implementation of DeepCluster')
 
-#The models in Caffe format expect BGR inputs that range in [0, 255]. 
-# You do not need to subtract the per-color-channel mean image since 
-# the preprocessing of the data is already included in our released models.
-dataloader = torch.utils.data.DataLoader(dataset,
-                            batch_size=args.batch,
-                            num_workers=args.workers,
-                            pin_memory=True)
+parser.add_argument('data', metavar='DIR', help='path to dataset')
+parser.add_argument('--arch', '-a', type=str, metavar='ARCH',
+                    choices=['alexnet', 'vgg16'], default='alexnet',
+                    help='CNN architecture (default: alexnet)')
+parser.add_argument('--sobel', action='store_true', help='Sobel filtering')
+parser.add_argument('--clustering', type=str, choices=['Kmeans', 'PIC'],
+                    default='Kmeans', help='clustering algorithm (default: Kmeans)')
+parser.add_argument('--nmb_cluster', '--k', type=int, default=10000,
+                    help='number of cluster for k-means (default: 10000)')
+parser.add_argument('--lr', default=0.05, type=float,
+                    help='learning rate (default: 0.05)')
+parser.add_argument('--wd', default=-5, type=float,
+                    help='weight decay pow (default: -5)')
+parser.add_argument('--reassign', type=float, default=1.,
+                    help="""how many epochs of training between two consecutive
+                    reassignments of clusters (default: 1)""")
+parser.add_argument('--workers', default=4, type=int,
+                    help='number of data loading workers (default: 4)')
+parser.add_argument('--epochs', type=int, default=200,
+                    help='number of total epochs to run (default: 200)')
+parser.add_argument('--start_epoch', default=0, type=int,
+                    help='manual epoch number (useful on restarts) (default: 0)')
+parser.add_argument('--batch', default=256, type=int,
+                    help='mini-batch size (default: 256)')
+parser.add_argument('--momentum', default=0.9, type=float, help='momentum (default: 0.9)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to checkpoint (default: None)')
+parser.add_argument('--checkpoints', type=int, default=25000,
+                    help='how many iterations between two checkpoints (default: 25000)')
+parser.add_argument('--seed', type=int, default=31, help='random seed (default: 31)')
+parser.add_argument('--exp', type=str, default='', help='path to exp folder')
+parser.add_argument('--verbose', action='store_true', help='chatty')
+
+
+def _store_feats(layer, inp, output):
+    """An ugly but effective way of accessing intermediate model features
+    """
+    _model_feats = []
+    _model_feats.append(np.reshape(output, (len(output), -1)).numpy())
+
+
+def compute_features(dataloader, model, N):
+    if args.verbose:
+        print('Compute features')
+    batch_time = AverageMeter()
+    end = time.time()
+    model.eval()
+    # discard the label information in the dataloader
+    for i, (input_tensor, _) in enumerate(dataloader):
+        input_var = torch.autograd.Variable(input_tensor.cuda())
+        aux = model(input_var).data.cpu().numpy()
+
+        if i == 0:
+            features = np.zeros((N, aux.shape[1]), dtype='float32')
+
+        aux = aux.astype('float32')
+        if i < len(dataloader) - 1:
+            features[i * args.batch: (i + 1) * args.batch] = aux
+        else:
+            # special treatment for final batch
+            features[i * args.batch:] = aux
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if args.verbose and (i % 200) == 0:
+            print('{0} / {1}\t'
+                  'Time: {batch_time.val:.3f} ({batch_time.avg:.3f})'
+                  .format(i, len(dataloader), batch_time=batch_time))
+    return features
+
+
+def get_activations(offset):
+    with torch.no_grad():
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                        std=[0.229, 0.224, 0.225])
+        tra = [transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize]
+        dataset = datasets.ImageFolder(offset, transform=transforms.Compose(tra))
+        dataloader = torch.utils.data.DataLoader(dataset,
+                                                batch_size=1,
+                                                num_workers=args.workers,
+                                                pin_memory=True)
+        
+        features = compute_features(dataloader, model, len(dataset))
+        layer_act = {}
+        for i,layer in enumerate(model.features):
+
+        #######  EMBED THIS CODE INTO THE PRETRAINED MODEL (code from eval_linear.py, line 196)
+        for m in model.features.modules():
+            if not isinstance(m, nn.Sequential):
+                x = m(x)
+            if isinstance(m, nn.ReLU):
+                if count == conv:
+                    return x
+            count = count + 1
+
+            layer_act[i] = layer.register_forward_hook(_store_feats)
+        
+        return layer_act
+
+
+if __name__ == '__main__':
+    global args
+    args = parser.parse_args()
+    modelpth = '/home/CUSACKLAB/annatruzzi/deepcluster_models/alexnet/'
+    checkpoint = torch.load(modelpth+'checkpoint.pth.tar')['state_dict']
+    checkpoint_new = OrderedDict()
+    for k, v in checkpoint.items():
+        name = k.replace(".module", '') # remove 'module.' of dataparallel
+        checkpoint_new[name]=v
+
+    model = models.alexnet(sobel=True, bn=True, out=10000) 
+    model.load_state_dict(checkpoint_new)
+    model.cuda()
+    #layers = ['ConvNdBackward1','ConvNdBackward2','ConvNdBackward3','ConvNdBackward4','ConvNdBackward5','ConvNdBackward9','ConvNdBackward13','ConvNdBackward16','ConvNdBackward19']
+    offsets = [x[0] for x in os.walk('/home/CUSACKLAB/annatruzzi/cichy2016/stimuli')]
+    act = {}
+    for offset in offsets:
+        act[offset] = get_activations(offset)
+    
+    with open('/home/CUSACKLAB/annatruzzi/cichy2016/cichy118_activations.pickle', 'wb') as handle:
+        pickle.dump(act, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 
-if __name__ == "__main__":
-    # change with deepcllustering layers
-    for layer in ['ConvNdBackward5','ConvNdBackward9','ConvNdBackward13','ConvNdBackward16','ConvNdBackward19']:
-    attribute_and_visualise(layer=layer)
+
+
 
 
