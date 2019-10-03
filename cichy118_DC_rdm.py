1ab51d5afe10eb91357741cba38715a518ca6152
Anna Truzzi <anna.truzzi91@gmail.com>
2019-10-03 15:30:12 +0000

    updates to rdms

--- cichy118_DC_rdm.py
+++ cichy118_DC_rdm.py
@@ -7,23 +7,44 @@ from scipy.spatial import distance
 from sklearn.manifold import MDS
 from statsmodels.stats.anova import AnovaRM
 from matplotlib import pyplot as plt
+import re
+from scipy.cluster.hierarchy import dendrogram, linkage
+import nltk
+from nltk.corpus import wordnet as wn
 
+## define images names
+img_names = {'001':'orange','002':'bench','003':'remote','004':'car','005':'stove','006':'man','007':'table','008':'apple','009':'chart','010':'dog','011':'fox','012':'bus','013':'train','014':'ipod',
+                '015':'pizza','016':'bird','017':'horse','018':'laptop','019':'bear','020':'basketball','021':'piano','022':'guitar','023':'baseball','024':'seal','025':'chair','026':'orangutan','027':'bowl',
+                '028':'tiger','029':'moped','030':'tie','031':'printer','032':'lion','033':'neil','034':'drum','035':'bow','036':'pomegranate','037':'butterfly','038':'lamp','039':'banana','040':'sofa',
+                '041':'lemon','042':'hoover','043':'hammer','044':'sunglasses','045':'bike','046':'rabbit','047':'measuring jug','048':'elephant','049':'microwave','050':'volleyball','051':'strawberry',
+                '052':'sheep','053':'frog','054':'washing machine','055':'fridge','056':'turtle','057':'axe','058':'helmet','059':'camel','060':'lipstick','061':'airplane','062':'dishwasher','063':'burger',
+                '064':'backpack','065':'purse','066':'hamster','067':'microphone','068':'mushroom','069':'cow','070':'violin','071':'orca','072':'cucumber','073':'termometer','074':'pineapple','075':'harp',
+                '076':'squirrel','077':'notebook','078':'trumpet','079':'plastic bag','080':'jug','081':'snail','082':'toaster','083':'benjo','084':'screwdriver','085':'snowbike','086':'pig',
+                '087':'pomegranate','088':'accordion','089':'tennis racket','090':'bagel','091':'trombone','092':'syringe','093':'fish','094':'hair drier','095':'starfish','096':'hot dog','097':'ladybug',
+                '098':'aircraft carrier','099':'jellyfish','100':'caffee machine','101':'television','102':'pretzel','103':'artichok','104':'golf ball','105':'alarm clock','106':'traffic light',
+                '107':'bottle','108':'pan','109':'weights','110':'football ball','111':'bellpepper','112':'corkscrew','113':'tennis ball','114':'computer mouse','115':'mug','116':'keyboard',
+                '117':'can opener','118':'fan'}
+
+img_synsets = {'001':'orange.n.01','002':'bench.n.01','003':'remote.n.01','004':'car.n.01','005':'stove.n.01','006':'man.n.01','007':'table.n.02','008':'apple.n.01','009':'cart.n.01','010':'dog.n.01',
+                '011':'fox.n.01','012':'bus.n.01','013':'train.n.01','014':'ipod.n.01','015':'pizza.n.01','016':'bird.n.01','017':'horse.n.01','018':'laptop.n.01','019':'bear.n.01','020':'basketball.n.02',
+                '021':'piano.n.01','022':'guitar.n.01','023':'baseball.n.01','024':'seal.n.09','025':'chair.n.01','026':'orangutan.n.01','027':'bowl.n.01','028':'tiger.n.02','029':'moped.n.01','030':'tie.n.01',
+                '031':'printer.n.03','032':'lion.n.01','033':'nail.n.02','034':'drum.n.01','035':'bow.n.04','036':'pomegranate.n.02','037':'butterfly.n.01','038':'lamp.n.02','039':'banana.n.02',
+                '040':'sofa.n.01','041':'lemon.n.01','042':'vacuum.n.04','043':'hammer.n.01','044':'sunglasses.n.02','045':'bike.n.02','046':'rabbit.n.01','047':'beaker.n.01','048':'elephant.n.01',
+                '049':'microwave.n.02','050':'volleyball.n.02','051':'strawberry.n.01','052':'sheep.n.01','053':'frog.n.01','054':'washer.n.03','055':'fridge.n.01','056':'turtle.n.01','057':'axe.n.01',
+                '058':'helmet.n.02','059':'camel.n.01','060':'lipstick.n.01','061':'airplane.n.01','062':'dishwasher.n.01','063':'burger.n.02','064':'backpack.n.01','065':'purse.n.01','066':'hamster.n.01',
+                '067':'microphone.n.01','068':'mushroom.n.05','069':'cow.n.02','070':'violin.n.01','071':'orca.n.01','072':'cucumber.n.02','073':'thermometer.n.01','074':'pineapple.n.02','075':'harp.n.01',
+                '076':'squirrel.n.01','077':'notebook.n.01','078':'trumpet.n.01','079':'plastic_bag.n.01','080':'pitcher.n.02','081':'snail.n.01','082':'toaster.n.02','083':'banjo.n.01',
+                '084':'screwdriver.n.01','085':'snowmobile.n.01','086':'pig.n.01','087':'pomegranate.n.02','088':'accordion.n.01','089':'racket.n.04','090':'bagel.n.01','091':'trombone.n.01',
+                '092':'syringe.n.01','093':'fish.n.01','094':'hair_dryer.n.01','095':'starfish.n.01','096':'hot_dog.n.03','097':'ladybug.n.01','098':'aircraft_carrier.n.01','099':'jellyfish.n.02',
+                '100':'coffee_maker.n.01','101':'television.n.03','102':'pretzel.n.01','103':'artichoke.n.01','104':'golf_ball.n.01','105':'alarm.n.04','106':'traffic_light.n.01',
+                '107':'bottle.n.01','108':'pan.n.01','109':'weight.n.02','110':'ball.n.01','111':'sweet_pepper.n.02','112':'corkscrew.n.01','113':'tennis_ball.n.01','114':'mouse.n.04','115':'mug.n.04',
+                '116':'keyboard.n.01','117':'can_opener.n.01','118':'fan.n.01'}
+
+## load activations dictionary 
 with open('/home/CUSACKLAB/annatruzzi/cichy2016/cichy118_activations.pickle','rb') as file:
     act=pickle.load(file)
 
-img_names = {'1':'orange','2':'bench','3':'remote','4':'car','5':'stove','6':'man','7':'table','8':'apple','9':'chart','10':'dog','11':'fox','12':'bus','13':'train','14':'ipod','15':'pizza','16':'bird',
-                '17':'horse','18':'laptop','19':'bear','20':'basketball','21':'piano','22':'guitar','23':'baseball','24':'seal','25':'chair','26':'orangutan','27':'bowl','28':'tiger','29':'moped',
-                '30':'tie','31':'printer','32':'lion','33':'neil','34':'drum','35':'bow','36':'pomgranade','37':'butterfly','38':'lamp','39':'banana','40':'sofa','41':'lemon','42':'hoover','43':'hammer',
-                '44':'sunglasses','45':'bike','46':'rabbit','47':'measuring jug','48':'elephant','49':'microwave','50':'volleyball','51':'strawberry','52':'sheep','53':'frog','54':'washing machine',
-                '55':'fridge','56':'turtle','57':'axe','58':'helmet','59':'camel','60':'lipstick','61':'airplane','62':'dishwasher','63':'burger','64':'backpack','65':'purse','66':'hamster','67':'microphone',
-                '68':'mushroom','69':'cow','70':'violin','71':'orca','72':'cucumber','73':'termometer','74':'pineapple','75':'harp','76':'squirrel','77':'notebook','78':'trumpet','79':'plastic bag',
-                '80':'jug','81':'snail','82':'toaster','83':'benjo','84':'screwdriver','85':'snowbike','86':'pig','87':'pomegranate','88':'accordion','89':'tennis racket','90':'bagel','91':'trombone',
-                '92':'syringe','93':'fish','94':'hair drier','95':'starfish','96':'hot dog','97':'ladybug','98':'aircraft carrier','99':'jellyfish','100':'caffee machine','101':'television','102':'pretzel',
-                '103':'artichok','104':'golf ball','105':'alarm clock','106':'traffic light','107':'bottle','108':'pan','109':'weights','110':'football ball','111':'bellpepper','112':'corkscrew',
-                '113':'tennis ball','114':'computer mouse','115':'mug','116':'keyboard','117':'can opener','118':'fan'}
-
-
-## create dict per layer
+## create one dictionary per layer
 layer1 = {}
 layer2 = {}
 layer3 = {}
@@ -37,13 +58,9 @@ for item in act.items():
     layer4[item[0]] = np.mean(np.mean(np.squeeze(item[1][3]),axis = 2), axis = 1)
     layer5[item[0]] = np.mean(np.mean(np.squeeze(item[1][4]),axis = 2), axis = 1)
 
+
+## transform layer dictionary in matrices ordered according to LCH
 orderedNames = act.keys()
-labels_dict = [label.split('/')[-1] for label in act.keys()]
-labels_img = []
-for key in labels_dict:
-    for item in img_names.items():
-        if item[0] in key:
-            labels_img.append(item[1])
 
 layer1_matrix = np.array([layer1[i] for i in orderedNames])
 layer2_matrix = np.array([layer2[i] for i in orderedNames])
@@ -52,15 +69,30 @@ layer4_matrix = np.array([layer4[i] for i in orderedNames])
 layer5_matrix = np.array([layer5[i] for i in orderedNames])
 
 
-rdm1 = distance.squareform(distance.pdist(layer1_matrix,metric='euclidean'))
-rdm2 = distance.squareform(distance.pdist(layer2_matrix,metric='euclidean'))
-rdm3 = distance.squareform(distance.pdist(layer3_matrix,metric='euclidean'))
-rdm4 = distance.squareform(distance.pdist(layer4_matrix,metric='euclidean'))
-rdm5 = distance.squareform(distance.pdist(layer5_matrix,metric='euclidean'))
+
+## calculate rdms
+rdm1 = distance.squareform(distance.pdist(layer1_matrix,metric='correlation'))
+rdm2 = distance.squareform(distance.pdist(layer2_matrix,metric='correlation'))
+rdm3 = distance.squareform(distance.pdist(layer3_matrix,metric='correlation'))
+rdm4 = distance.squareform(distance.pdist(layer4_matrix,metric='correlation'))
+rdm5 = distance.squareform(distance.pdist(layer5_matrix,metric='correlation'))
+
+
+
+## get list of lables for the plots
+labels_dict = [label.split('/')[-1] for label in orderedNames]
+labels_img = []
+for key in labels_dict:
+    for item in img_names.items():
+        if item[0] in key:
+            print item[0]
+            print key
+            labels_img.append(item[1])
+
 
 fig=plt.figure()
 ax = fig.add_subplot(111)
-plt.imshow(rdm5,vmin=0,vmax=3)
+plt.imshow(rdm1,vmin=0,vmax=0.2)
 plt.colorbar()
 ticks = np.arange(0,118,1)
 ax.set_xticks(ticks)
@@ -69,4 +101,63 @@ ax.set_xticklabels(labels_img,rotation = 90, fontsize = 8)
 ax.set_yticklabels(labels_img,fontsize = 8)
 plt.show()
 
+
+
+#########   Multiplot
+fig=plt.figure()
+ax = fig.add_subplot(3,2,1)
+plt.imshow(rdm1,vmin=0,vmax=0.2)
+plt.colorbar()
+ticks = np.arange(0,118,1)
+ax.set_xticks(ticks)
+ax.set_yticks(ticks)
+ax.set_xticklabels(labels_img,rotation = 90, fontsize = 8)
+ax.set_yticklabels(labels_img,fontsize = 8)
+
+ax = fig.add_subplot(3,2,2)
+plt.imshow(rdm2,vmin=0,vmax=1)
+plt.colorbar()
+ticks = np.arange(0,118,1)
+ax.set_xticks(ticks)
+ax.set_yticks(ticks)
+ax.set_xticklabels(labels_img,rotation = 90, fontsize = 8)
+ax.set_yticklabels(labels_img,fontsize = 8)
+
+ax = fig.add_subplot(3,2,3)
+plt.imshow(rdm3,vmin=0,vmax=1)
+plt.colorbar()
+ticks = np.arange(0,118,1)
+ax.set_xticks(ticks)
+ax.set_yticks(ticks)
+ax.set_xticklabels(labels_img,rotation = 90, fontsize = 8)
+ax.set_yticklabels(labels_img,fontsize = 8)
+
+ax = fig.add_subplot(3,2,4)
+plt.imshow(rdm4,vmin=0,vmax=1)
+plt.colorbar()
+ticks = np.arange(0,118,1)
+ax.set_xticks(ticks)
+ax.set_yticks(ticks)
+ax.set_xticklabels(labels_img,rotation = 90, fontsize = 8)
+ax.set_yticklabels(labels_img,fontsize = 8)
+
+ax = fig.add_subplot(3,2,5)
+plt.imshow(rdm5,vmin=0,vmax=1.2)
+plt.colorbar()
+ticks = np.arange(0,118,1)
+ax.set_xticks(ticks)
+ax.set_yticks(ticks)
+ax.set_xticklabels(labels_img,rotation = 90, fontsize = 8)
+ax.set_yticklabels(labels_img,fontsize = 8)
+plt.show()
+
+
+
+plt.figure(figsize=(10, 7))
+dendrogram(rdm1,
+            orientation='top',
+            labels=labels_img,
+            distance_sort='descending',
+            show_leaf_counts=True)
+plt.show()
 
