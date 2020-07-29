f24b4b437b17cf25dd5de20bfa8f5de9d3e6cad8
Anna Truzzi <anna.truzzi91@gmail.com>
2020-07-29 10:38:06 +0100

    fixed EOF error in main.sh

--- main.sh
+++ main.sh
@@ -15,7 +15,7 @@ K=10
 WORKERS=12
 PYTHON="/opt/anaconda3/envs/dc_p27/bin/python"
 CHECKPOINTS=5005
-RESUME="/home/annatruzzi/checkpoints/multiple_dc_instantiations/dc_1/checkpoint_dc1_epoch135.pth.tar
+RESUME="/home/annatruzzi/checkpoints/multiple_dc_instantiations/dc_1/checkpoint_dc1_epoch135.pth.tar"
 
 for i in 1
 do
