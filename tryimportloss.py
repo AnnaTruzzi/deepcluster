5e25e5f2093bdc01671a95790de1f9defb17580f
Anna Truzzi <anna.truzzi91@gmail.com>
2020-09-04 15:35:08 +0100

    open the log file failed, back to basics

--- tryimportloss.py
+++ tryimportloss.py
@@ -1,8 +1,7 @@
 import pickle
 import numpy as np
-import pandas as pd
 
-pth = /home/annatruzzi/deepcluster_eval/dc_1/log/loss_log
+pth = '/home/annatruzzi/deepcluster_eval/dc_1/log/loss_log'
 data = pickle.load(pth)
 print(data)
  
