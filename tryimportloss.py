95347d75e338582a147229db91e3a96c260c4111
Anna Truzzi <anna.truzzi91@gmail.com>
2020-09-16 13:35:25 +0100

    directory fixed

--- tryimportloss.py
+++ tryimportloss.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt
 import glob
 import os
 
-def main():
+def main(pth):
     loss_list = []
-    for directory in glob.glob(pth):
+    for directory in glob.glob(pth+'*'):
         print(directory)
         filename = os.path.join(directory,'log/loss_log')
         print(filename)
         with open (filename,'rb') as f:
             data = pickle.load(f)
             loss_list.append(data[-1])
-            print(data.shape)
+            print(len(data))
     plt.plot(loss_list)
     plt.savefig('LossPlot_dc1.png',bbox_inches='tight')
     plt.close()
@@ -22,4 +22,4 @@ def main():
  
 if __name__ == '__main__':
     pth = '/home/annatruzzi/deepcluster_eval/dc_1/'
-    main()
+    main(pth)
