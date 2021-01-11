5a5270e15122082f2ecbcf67c8db350deae6cd2f
Anna Truzzi <anna.truzzi91@gmail.com>
2021-01-11 11:27:54 +0000

    trying to fix the everlasting issue with the instance variable

--- launch_dependencies_resumeonly.py
+++ launch_dependencies_resumeonly.py
@@ -10,7 +10,9 @@ args = parser.parse_args()
 
 # submit the first job
 epoch = 1
-cmd = "sbatch --export=INSTANTIATION=$%s,EPOCH_RES=$%s launch_resume.sh" % (args.instantiation, epoch)
+INSTANTIATION = args.instantiation
+#cmd = "sbatch --export=INSTANTIATION=$%d,EPOCH_RES=$%d launch_resume.sh" % (args.instantiation, epoch)
+cmd = "sbatch --export=ALL launch_resume.sh"
 status, jobnum = commands.getstatusoutput(cmd)
 if (status == 0 ):
     print("Job1 is %s" % jobnum)
@@ -18,7 +20,8 @@ else:
     print("Error submitting Job1 %s" % jobnum)
 
 for epoch in range(2,500):
-    cmd = "sbatch --dependency==afterok:%s --export=INSTANTIATION=$%s,EPOCH_RES=$%s launch_resume.sh " % (jobnum, args.instantiation, epoch)
+    #cmd = "sbatch --dependency==afterok:%s --export=INSTANTIATION=$%d,EPOCH_RES=$%d launch_resume.sh " % (jobnum, args.instantiation, epoch)
+    cmd = "sbatch --dependency==afterok:%s --export=ALL launch_resume.sh " % jobnum
     status,jobnum = commands.getstatusoutput(cmd)
     if (status == 0 ):
         print("Running job is %s" % jobnum)
